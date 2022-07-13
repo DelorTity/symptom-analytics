3588e1627cdaa40517cb079ce1907cb56f7a28c8
ElDelor <delortity@gmail.com>
2022-07-13 18:42:03 +0100

    Recuperation de la liste de symptoms depuis un fichier

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,10 +3,14 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.List;
 
 public class AnalyticsCounter {
 
 	public static void main(String args[]) throws Exception {
+		//lecture des symptomes
+		ISymptomReader symptomReader = new ReadSymptomDataFromFile("symptoms.txt");
+		List<String> symptoms = symptomReader.getSymptoms();
 
 	}
 }
